7260a11446c42c5d534e8bb801bad89553ac4d7b
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2026-05-04 14:15:34 -0300

    feat projeto integrado com BRASIL API

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
 //O navegador(no caso hoje 28/04/26, CHROME) fez a requisição HTTP
 
 async function getDadosCep(cep) {
-    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const url = `https://brasilapi.com.br/api/cep/v1/${cep}`//`https://viacep.com.br/ws/${cep}/json/`
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data)
     return data
 }
 
@@ -14,10 +15,10 @@ async function preencherFormulario () {
     const cep = document.getElementById('cep').value
     const dadosCep = await getDadosCep(cep)
 
-    document.getElementById('endereco').value   =   dadosCep.logradouro
-    document.getElementById('cidade').value     =   dadosCep.localidade
-    document.getElementById('estado').value     =   dadosCep.uf
-    document.getElementById('bairro').value     =   dadosCep.bairro
+    document.getElementById('endereco').value   =   dadosCep.street
+    document.getElementById('cidade').value     =   dadosCep.city
+    document.getElementById('estado').value     =   dadosCep.state
+    document.getElementById('bairro').value     =   dadosCep.neighborhood
 }
 
 document.getElementById('cep').addEventListener('focusout', preencherFormulario)
