9533335c03274a655199038a3b50cac02d8cf15e
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2026-05-04 14:19:16 -0300

    comentários

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ async function getDadosCep(cep) {
     const url = `https://brasilapi.com.br/api/cep/v1/${cep}`//`https://viacep.com.br/ws/${cep}/json/`
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data)
+    //Precisei de um console.log para realizar o debug e encontrar o objeto json diferente da API VIACEP correto
+    // console.log(data)
     return data
 }
 
 async function preencherFormulario () {
     const cep = document.getElementById('cep').value
     const dadosCep = await getDadosCep(cep)
-
+    //Brasil Api responde um json com objetos em inglês, por isso os dados do cep estão com atributos assim:
     document.getElementById('endereco').value   =   dadosCep.street
     document.getElementById('cidade').value     =   dadosCep.city
     document.getElementById('estado').value     =   dadosCep.state
